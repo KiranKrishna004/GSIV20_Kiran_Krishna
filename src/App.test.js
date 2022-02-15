9343feae6601254fe9630ce9dc38efa9e6bf2675
KiranKrishna004 <kiran.004narath@gmail.com>
2022-02-15 15:19:04 +0530

    App testing Added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/** @format */
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// /** @format */
+
+// import App from "./App";
+// import TestRenderer from "react-test-renderer";
+// import { Provider } from "react-redux";
+// import "@testing-library/jest-dom/extend-expect";
+// import pageReducer from "./reducers/pageReducer";
+// import { createStore } from "redux";
+// import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+// import MovieCard from "./components/MovieCard";
+
+// const pageStore = createStore(pageReducer);
+// test("renders ", () => {
+// 	const testRenderer = TestRenderer.create(<App />);
+// 	const testInstance = testRenderer.root;
+// 	console.log("JSON: ", testRenderer.toTree());
+// 	const list = testInstance.findAllByType(MovieCard);
+
+// 	expect(list.length).toBe(20);
+// });
+/** @format */
+import App from "./App";
+import TestRenderer from "react-test-renderer";
+import "@testing-library/jest-dom/extend-expect";
+// import { render, screen } from "@testing-library/react";
+test("renders ", () => {
+	const testRenderer = TestRenderer.create(<App />);
+	console.log(testRenderer.toTree());
 });
